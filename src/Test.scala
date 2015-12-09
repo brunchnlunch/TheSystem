b403d2886774132f588d60d1aab78f248087b47e
brunchnlunch <charlie.anglin@gmail.com>
2015-12-09 11:22:08 +0000

    pasted in SystemAttempt

--- src/Test.scala
+++ src/Test.scala
@@ -1,7 +1,34 @@
-
+import scala.collection.mutable.ArrayBuffer   //mutable arrays
 
 object Test {
     def main(args: Array[String]) {
-     println("asdasd")
-   }
+    
+    
+    case class Item (ID: Int, Name: String, Quantity: Int, Volume: Int, Location: Int)
+    
+    object StockList {
+    var stocklist = ArrayBuffer(Item(100, "ChristmasGnome", 40, 25, 11111),
+        Item(101, "BigGnome", 40, 50, 11112),
+        Item(102, "TinyGnome", 60, 10, 11113))
+    
+        
+    def OrderStockListByQuantity  {
+      StockList.stocklist = StockList.stocklist.sortWith(_.Quantity > _.Quantity)
+    }
+    
+    def OrderStockListByName : ArrayBuffer[Item] = {
+      StockList.stocklist.sortWith(_.Name < _.Name)
+    }
+    
+    }
+    
+    StockList
+    println(StockList.stocklist(0).Quantity)
+    println(StockList.stocklist)
+    StockList.OrderStockListByQuantity
+    println(StockList.stocklist)
+    
+    
+    
+  }
 }
